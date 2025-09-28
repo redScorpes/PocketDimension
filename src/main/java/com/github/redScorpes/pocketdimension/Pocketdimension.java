64909176840df91d5111c4d5dcba8c505d976657
redScorpes <roter-skorpion@mail.de>
2025-09-28 16:48:43 +0200

    first commit

---
+++ src/main/java/com/github/redScorpes/pocketdimension/Pocketdimension.java
@@ -0,0 +1,10 @@
+package com.github.redScorpes.pocketdimension;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Pocketdimension implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/github/redScorpes/pocketdimension/client/PocketdimensionClient.java
@@ -0,0 +1,10 @@
+package com.github.redScorpes.pocketdimension.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PocketdimensionClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
